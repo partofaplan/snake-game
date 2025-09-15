7ba03639d45013eea5d5734b3b431b9ccd3e4aec
Zach Perkins <partofaplan@gmail.com>
2025-09-14 21:40:01 -0500

    Updated d-pad

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE = 'snake-cache-v1';
+const CACHE = 'snake-cache-v2';
 const ASSETS = [
   '/',
   '/index.html',
@@ -38,4 +38,3 @@ self.addEventListener('fetch', (e) => {
     })
   );
 });
-
